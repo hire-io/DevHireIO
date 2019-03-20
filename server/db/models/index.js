a4b15d3337729bbc8a9a824de34e0b52d06a1a8f
Shmuel Lotman <shmuel@adhocteam.us>
2019-03-19 23:45:12 -0400

    workExperience table

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./user')
 const InterviewQuestion = require('./interviewQuestion')
+const WorkExperience = require('./workExperience')
 const Skills = require('./skills')
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -7,7 +8,8 @@ const Skills = require('./skills')
  *
  *    BlogPost.belongsTo(User)
  */
-
+Skills.belongsToMany(User, { through: 'UserSkills' })
+User.belongsToMany(Skills, { through: 'UserSkills' })
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -17,5 +19,6 @@ const Skills = require('./skills')
 module.exports = {
   User,
   InterviewQuestion,
+  WorkExperience,
   Skills
 }
